f6540b97cd56913af829dee809b44358a16e15af
osechitan <osechitan@gmail.com>
2021-01-19 17:13:51 +0900

    Chapter2完了

---
+++ Chapter2/main.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        list: []
+    },
+    created: function() {
+        axios.get('list.json').then(function(response) {
+            this.list = response.data
+        }.bind(this)).catch(function(e) {
+            console.error(e)
+        })
+    }
+})
